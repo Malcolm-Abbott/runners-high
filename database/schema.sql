4e7ee68f96ba06aa2a8e04f00bb36b5b07698452
Malcolm Abbott <mjacoding@gmail.com>
2024-04-13 00:18:24 +0000

    Initial commit

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,24 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "public"."users" (
+  "userId" serial PRIMARY KEY,
+  "username" text NOT NULL,
+  "password" text NOT NULL,
+  "created_at" timestamptz(6) NOT NULL DEFAULT now()
+);
+
+CREATE TABLE "public"."runs" (
+  "runId" serial PRIMARY KEY,
+  "userId" integer NOT NULL,
+  "distanceRun" text NOT NULL,
+  "runDuration" integer NOT NULL,
+  "averageHeartRate" integer NOT NULL,
+  "runDate" text NOT NULL,
+  "photoUrl" text NOT NULL,
+  "createdAt" timestamptz(6) NOT NULL DEFAULT now()
+);
+
+ALTER TABLE ONLY "public"."runs"
+  ADD CONSTRAINT user_id_fkey FOREIGN KEY ("userId") REFERENCES "public"."users"("userId") ON UPDATE CASCADE ON DELETE RESTRICT;
