070df4b94e40d45d4cc4c4fa420f50bbd5d1f3c6
Malcolm Abbott <mjacoding@gmail.com>
2024-04-21 19:44:21 +0000

    Submit handler with post fetch request for SignUp.tsx complete, successfully creating users in the database

--- client/src/Pages/SignUp.tsx
+++ client/src/Pages/SignUp.tsx
@@ -1,3 +1,5 @@
+import { useState, FormEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Heading } from '../Components/Heading';
 import { LoginImage } from '../Components/LoginImage';
 import { PasswordInput } from '../Components/PasswordInput';
@@ -5,8 +7,40 @@ import { TextInput } from '../Components/TextInput';
 import { Button } from '../Components/Button';
 
 export function SignUp() {
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  async function handleSubmit(
+    event: FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    event.preventDefault();
+    try {
+      setIsLoading(true);
+      const formData = new FormData(event.currentTarget);
+      const userData = Object.fromEntries(formData);
+      const req = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(userData),
+      };
+      const res = await fetch('/api/sign-up', req);
+      if (!res.ok) throw new Error(`fetch error ${res.status}`);
+      const user = await res.json();
+      alert(
+        `Successfully registered ${user.username} as a new Runners High member!`
+      );
+      navigate('/');
+    } catch (err) {
+      alert(`Error registering user: ${err}`);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="container grid place-items-center gap-8 my-8">
         <Heading title="Sign Up" />
         <TextInput label="Username" category="username" />
