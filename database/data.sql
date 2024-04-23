8253236ed19142a29925b056fd66b60989b9b51f
Malcolm Abbott <mjacoding@gmail.com>
2024-04-23 17:20:13 +0000

    added guest user information to the deployed database

--- database/data.sql
+++ database/data.sql
@@ -9,3 +9,6 @@
 --      ('Learn to code', false),
 --      ('Build projects', false),
 --      ('Get a job', false);
+
+insert into "users" ("username", "password")
+  values('guest', '$argon2id$v=19$m=4096,t=3,p=1$/AOCb+P7RhZkckMGMp6BLQ$ONQhbw8xKxy6RKm2FBVqXotVxOkJMLaZWufH4DwfWsc');
